682b189ba94a0b00e3e17e14a90102245e9d4a39
James Tan <tan_yu_kai_james@s2022.ssts.edu.sg>
2023-07-18 16:44:41 +0800

    commit

--- assets/script.js
+++ assets/script.js
@@ -29,28 +29,29 @@ document.getElementById("cards").onmousemove = e => {
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-let interval = null;
+    let interval = null;
 
-document.querySelector(".sinc").onmouseover = event => {  
-  let iteration = 0;
+    document.querySelector(".sinc").onmouseover = event => {  
+      let iteration = 0;
   
-  clearInterval(interval);
-  
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if(index < iteration) {
-          return event.target.dataset.value[index];
-        }
-        return letters[Math.floor(Math.random() * 26)]
-      })
-      .join("");
-    
-    if(iteration >= event.target.dataset.value.length){ 
       clearInterval(interval);
-    }
+  
+      interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+          .split("")
+          .map((letter, index) => {
+            if(index < iteration) {
+              return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 26)]
+          })
+          .join("");
     
-    iteration += 1 / 3;
-  }, 60);
-}
+        if(iteration >= event.target.dataset.value.length){ 
+          clearInterval(interval);
+        }
+    
+        iteration += 1 / 3;
+      }, 60);
+    }
+
